fb8e010356a325b991fa8bb9eefac63ce5ee83b7
etxacam <etxacam@SE-00014598.ericsson.se>
2019-03-07 15:58:45 +0100

    Fix performance issue when the request clear extended data

--- plugins/uml/diagram/org.eclipse.papyrus.uml.diagram.sequence/runtime-src/org/eclipse/papyrus/uml/diagram/sequence/runtime/interactiongraph/InteractionGraphRequestHelper.java
+++ plugins/uml/diagram/org.eclipse.papyrus.uml.diagram.sequence/runtime-src/org/eclipse/papyrus/uml/diagram/sequence/runtime/interactiongraph/InteractionGraphRequestHelper.java
@@ -15,6 +15,10 @@
 
 package org.eclipse.papyrus.uml.diagram.sequence.runtime.interactiongraph;
 
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+
 import org.eclipse.gef.GraphicalEditPart;
 import org.eclipse.gef.Request;
 import org.eclipse.gmf.runtime.emf.type.core.requests.IEditCommandRequest;
@@ -42,7 +46,6 @@ public class InteractionGraphRequestHelper {
 		if (graph != null) {
 			return graph;
 		}
-
 		graph = createInteractionGraph(gep);
 		if (graph == null) {
 			return null;
@@ -74,8 +77,13 @@ public class InteractionGraphRequestHelper {
 		return InteractionGraphFactory.getInstance().createInteractionGraph((Interaction) el, dia, gep.getViewer());
 	}
 
-	@SuppressWarnings("unchecked")
+	@SuppressWarnings({ "unchecked", "rawtypes" })
 	public static void bound(Request req, InteractionGraph graph) {
+		Map extendedData = req.getExtendedData();
+		if (!(extendedData instanceof ExtendedData)) {
+			extendedData = new ExtendedData(extendedData);
+			req.setExtendedData(extendedData);
+		}
 		req.getExtendedData().put(REQ_PROPERTY, graph);
 	}
 
@@ -83,11 +91,116 @@ public class InteractionGraphRequestHelper {
 		req.setParameter(REQ_PROPERTY, graph);
 	}
 
+	@SuppressWarnings("rawtypes")
 	public static void unbound(Request req, InteractionGraph graph) {
+		Map extendedData = req.getExtendedData();
+		if (extendedData instanceof ExtendedData) {
+			extendedData = ((ExtendedData)extendedData).delegate;
+			req.setExtendedData(extendedData);
+		}
 		req.getExtendedData().remove(REQ_PROPERTY, graph);
 	}
 
 	public static void unbound(IEditCommandRequest req, InteractionGraph graph) {
 		req.setParameter(REQ_PROPERTY, null);
+	}
+	
+	@SuppressWarnings({"unchecked", "rawtypes", "unused" })
+	private static class ExtendedData implements Map {
+		private InteractionGraph graph;
+		private Map delegate;
+		
+		public ExtendedData(Map delegate) {
+			super();
+			this.delegate = delegate;
+		}
+
+		@Override
+		public int size() {
+			checkAndAdd();
+			return delegate.size();
+		}
+
+		@Override
+		public boolean isEmpty() {
+			checkAndAdd();
+			return delegate.isEmpty();
+		}
+
+		@Override
+		public boolean containsKey(Object key) {
+			checkAndAdd();
+			return delegate.containsKey(key);
+		}
+
+		@Override
+		public boolean containsValue(Object value) {
+			checkAndAdd();
+			return delegate.containsValue(value);
+		}
+
+		@Override
+		public Object get(Object key) {
+			checkAndAdd();
+			return delegate.get(key);
+		}
+
+		@Override
+		public Object put(Object key, Object value) {
+			if (key.equals(REQ_PROPERTY)) {
+				graph = (InteractionGraph)value;
+				if (graph == null)
+					delegate.remove(REQ_PROPERTY);
+			}
+			checkAndAdd();
+			return delegate.put(key, value);
+		}
+
+		@Override
+		public Object remove(Object key) {
+			if (key.equals(REQ_PROPERTY)) {
+				graph = null;
+				delegate.remove(REQ_PROPERTY);
+			}
+			checkAndAdd();
+			return delegate.remove(key);
+		}
+
+		@Override
+		public void putAll(Map m) {
+			checkAndAdd();
+			delegate.putAll(m);
+		}
+
+		@Override
+		public void clear() {
+			checkAndAdd();
+			delegate.clear();
+		}
+
+		@Override
+		public Set keySet() {
+			checkAndAdd();
+			return delegate.keySet();
+		}
+
+		@Override
+		public Collection values() {
+			checkAndAdd();
+			return delegate.values();
+		}
+
+		@Override
+		public Set<Entry> entrySet() {
+			checkAndAdd();
+			return delegate.entrySet();
+		}
+		
+		private void checkAndAdd() {
+			if (graph != null && !delegate.containsKey(REQ_PROPERTY)) {
+				delegate.put(REQ_PROPERTY, graph);
+			}
+		}
+
 	}
 }
