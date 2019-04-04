9e502c9678bf5af993888ee61bd4d499fcbf7a31
etxacam <antonio.campesino.robles@ericsson.com>
2019-04-04 11:42:37 +0200

    Update links to absolute

--- js/content.js
+++ js/content.js
@@ -12,6 +12,8 @@
 		htmlString += `<li><img src="${base}/img/folder.png"/> <a href="${base}${file.path}">${file.name}</a></li>`;
 	}
 	for (let file of data) {
+	  if (file.name.indexOf('.html') == (file.name.length - 5) || file.name.indexOf('.bak') == (file.name.length - 4))
+	  	continue;
 	  if (file.type == 'file')
 		  htmlString += `<li><img src="${base}/img/file.png"/> <a href="${base}${file.path}">${file.name}</a></li>`;
 	}
